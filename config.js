a8dad953aab16ce7df1ece9dcbef2b42d417ca73
Sdeyy <soysdeyy@gmail.com>
2025-05-18 02:54:32 -0300

    Removed unnecesary code

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    botClientID: "YOUR_BOT_ID",
-    botPrefix: "!",
-    ownerID: "YOUR_ID",
-    embedColor: "#00dbff",
-    embedfooterText: "Discord.js v14 Example by @Buddier"
-}
